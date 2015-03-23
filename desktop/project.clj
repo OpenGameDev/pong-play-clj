d36217415d28d84a54d4af2cdf35ba54519d1f50
Andre Anastacio <andreluisanastacio@gmail.com>
2015-03-22 23:45:52 -0300

    Bump up play-clj version

--- desktop/project.clj
+++ desktop/project.clj
@@ -1,6 +1,6 @@
 (defproject pong "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.5.3"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.5.3"]
                  [com.badlogicgames.gdx/gdx-box2d "1.5.3"]
@@ -12,8 +12,8 @@
                  [com.badlogicgames.gdx/gdx-platform "1.5.3"
                   :classifier "natives-desktop"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.4.4"]]
-  
+                 [play-clj "0.4.5"]]
+
   :source-paths ["src" "src-common"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [pong.core.desktop-launcher]
