e0f38586a085f97ef128a17adb6fce3dc086a7ce
Andre Anastacio <andreluisanastacio@gmail.com>
2015-03-22 23:46:11 -0300

    Make easy the code reload in runtime

--- desktop/src-common/pong/core.clj
+++ desktop/src-common/pong/core.clj
@@ -1,19 +1,30 @@
 (ns pong.core
   (:require [play-clj.core :refer :all]
+            [play-clj.g2d :refer :all]
             [play-clj.ui :refer :all]))
+
+(use 'play-clj.repl)
+(declare pong main-screen)
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen :renderer (stage))
-    (label "Hello World" (color :white)))
+    (label "foo" (color :white)))
 
   :on-render
   (fn [screen entities]
     (clear!)
-    (render! screen entities)))
+    (render! screen entities))
+
+  :on-key-down
+  (fn [screen entities]
+    (cond
+     (key-pressed? :r) (set-screen! pong main-screen))))
 
 (defgame pong
   :on-create
   (fn [this]
     (set-screen! this main-screen)))
+
+(e main-screen)
